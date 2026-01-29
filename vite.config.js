d73ff2ce89fb574cc9b8c87505e2854400937d0d
Ankita Yadav <ankitayadav476076@gmail.com>
2026-01-29 23:50:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
+=======
+  base:"/portfolio",
+>>>>>>> 9549fc1 (first commit)
   plugins: [react(),tailwindcss()],
 })
